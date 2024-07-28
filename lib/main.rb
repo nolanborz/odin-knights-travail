8eeb9bd64a152c497026f26d5451795e091ace55
nolanborz <nolanrborzoni@gmail.com>
2024-07-28 21:57:40 +0900

    Build classes to begin bidirectional search approach

--- lib/main.rb
+++ lib/main.rb
@@ -2,7 +2,6 @@ require_relative 'knight-moves'
 class Knight
   include KnightMoves
   attr_reader :position
-
   def initialize(x, y)
     @position = [x, y]
   end
@@ -15,27 +14,34 @@ class Knight
   end
   
   def valid_pos?(x, y)
-    if x < 0 || y < 0 || x >= 8 || y >= 8
-      false
-    else
-      true
-    end
+    x.between?(0, 7) && y.between?(0, 7)
   end
 
   def possible_moves
     moves = []
     original_position = @position.dup
 
-    [:move_up_right, :move_up_left, :move_right_up, :move_right_down, :move_down_right, :move_down_left, :move_left_down, :move_left_up].each do |move|
+    [:move_up_right, :move_up_left, :move_right_up, :move_right_down, :move_down_right, :move_down_left, :move_left_down, :move_left_up].filter_map do |move|
       send(move)
-      moves << @position.dup
+      new_pos = @position.dup
       @position = original_position.dup
-     end
-     moves.select {|pos| valid_pos?(pos[0], pos[1])}
+      new_pos if valid_pos?(*new_pos)
+    end
   end
 end
+class ChessPathFinder
+  def find_shortest_path(start, target, movement_rules)
+  end
 
-horse = Knight.new(0, 0)
-horse.print_position
-end_positions = horse.possible_moves
-puts end_positions.inspect
+  private
+
+  def knight_moves
+    [
+      [1, 2], [2, 1], [2, -1], [1, -2],
+      [-1, -2], [-2, -1], [-2, 1], [-1, 2]
+    ]
+  end
+
+knight = Knight.new(0, 0)
+path_finder = ChessPathFinder.new
+shortest_path = path_finder.find_shortest_path(knight.position, [3, 3], method(:knight_moves))
